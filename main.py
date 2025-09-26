3b1bec663dcc16e0f8a96211c997697bbde4069f
Solstice Brewis <N1291969@my.ntu.ac.uk>
2025-09-26 11:44:35 +0100

    Correct information is printed

--- main.py
+++ main.py
@@ -2,19 +2,17 @@
 
 import requests #requests the HTML from websites
 from bs4 import BeautifulSoup #makes the parsing and searching of the HTML (or XML) more intuative
-from lxml import etree
 
 
 def scrape():
     url = 'https://www.bbcgoodfood.com/recipes/chipotle-sweet-potato-black-bean-stew-cheddar-dumplings'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    dom = etree.HTML(str(soup))
-    search(dom)
+    search(soup)
 
-def search(dom):
-    text = dom.xpath('/html/body/div[1]/div[5]/main/div[2]/div/div[3]/div[1]/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/section/section')[0].text
-    print(text)
+def search(soup):
+    ingredients = soup.find('ul', {'class': 'ingredients-list list'}).text
+    print(ingredients)
 
 
 if __name__ == '__main__':
